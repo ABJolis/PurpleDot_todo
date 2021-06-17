9ae564e2d43b931de046a53c37d62fb8b077279c
Anne Berg Jolis <annejolis@gmail.com>
2021-06-18 00:10:15 +0100

    feat: put request to update todo description now works

--- server/router.js
+++ server/router.js
@@ -50,10 +50,24 @@ router.get('/todos/status', async(req, res) => {
   }
 })
 
-
 //filter todos by isComplete status
 
 //update a todo description
+router.put('/todos/:id', async(req, res)=> {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+    "UPDATE todo SET description = $1 WHERE todo_id = $2",
+    [description, id]
+    );
+    res.json('Todo was updated!')
+  } catch (error) {
+    console.error('error: ', error.message);
+  }
+})
+
+// update a todo's isComplete status
 
 //delete a todo
 
